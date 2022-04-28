b0327f75449c998db384b832eb9ae7eb782928e2
YourRentIsDue <simas.lyn@gmail.com>
2022-04-28 12:25:12 +0100

    Added some things

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,20 @@
 use eframe::{egui, epi};
+use chrono::prelude::*;
+
+
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
+
+pub struct Ban {
+    year: i32,
+    month: i32,
+    day: i32,
+    minute: i32,
+    hour: i32,
+}
+
 pub struct TemplateApp {
     // Example stuff:
     label: String,
@@ -10,6 +22,10 @@ pub struct TemplateApp {
     // this how you opt-out of serialization of a member
     #[cfg_attr(feature = "persistence", serde(skip))]
     value: f32,
+    popup: bool,
+    username: String,
+    is_banned: bool,
+    ban: Option<Ban>,
 }
 
 impl Default for TemplateApp {
@@ -18,9 +34,16 @@ impl Default for TemplateApp {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            popup: false,
+            username: "".to_owned(),
+            is_banned: false,
+            ban: None,
         }
     }
 }
+
+
+
 
 impl epi::App for TemplateApp {
     fn name(&self) -> &str {
@@ -40,6 +63,7 @@ impl epi::App for TemplateApp {
         if let Some(storage) = _storage {
             *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
         }
+
     }
 
     /// Called by the frame work to save state before shutdown.
@@ -49,70 +73,58 @@ impl epi::App for TemplateApp {
         epi::set_value(storage, epi::APP_KEY, self);
     }
 
+    
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
-        let Self { label, value } = self;
+        let Self { label, value, popup, username, is_banned, ban } = self;
 
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+        
+        
+        let window = egui::Window::new("Add");
 
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        frame.quit();
-                    }
-                });
+        egui::SidePanel::left("add_acc_panel")
+            .show(ctx, |ui| {
+                ui.add_space(3.0);
+                if ui.add_sized([100.0, 40.0], egui::Button::new("Add account")).clicked() {
+                    self.popup = true;
+                }
             });
-        });
-
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
-            });
-        });
+        
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
+
+            if self.popup {
+                window.open(&mut self.popup).default_height(20.0).show(ctx, |ui| {
+                    ui.with_layout(egui::Layout::left_to_right(), |ui| {
+                        ui.add_sized([100.0, 20.0], egui::TextEdit::singleline(&mut self.username).hint_text("username"));
+                        ui.checkbox(&mut self.is_banned, "Is this account currently banned?");
+                        
+                    });
+                    //ui.add_space(50.0)
+
+                    if self.is_banned {
+                        //this is where I'd like two more TextEdits to go ideally.
+                    }
+                    
+                   
+                    
+                    
+                });
+            }
         });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
+        
+
+        
+        
+        
+
+
+
+
+
+
     }
 }
